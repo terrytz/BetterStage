82c0bef02a54d136ef88dcee616f6d431b0242fd
terrytz <tz@terryzhang.com>
2026-07-30 06:19:19 +0800

    Update BetterStage to 1.3.4

--- Casks/betterstage.rb
+++ Casks/betterstage.rb
@@ -1,6 +1,6 @@
 cask "betterstage" do
-  version "1.3.3"
-  sha256 "9e211db0d9a428afc358c72048da95e62b4f92dc56bd7770a50380d451bf9760"
+  version "1.3.4"
+  sha256 "be18a831a00b4ef76c74bca052c6b2f065987033ea91a39a1197c56011e44e8d"
 
   url "https://update.betterstage.app/BetterStage-#{version}.dmg"
   name "BetterStage"
